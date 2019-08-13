3b5ceea485891af809bca3c708133c3241599071
Carol Richardson <Carol.Richardson@alaskaair.com>
2019-08-13 11:21:38 -0700

    added more parts of the toasts

--- src/ods-toast.js
+++ src/ods-toast.js
@@ -13,21 +13,22 @@ import styleCss from "./style-css.js";
 
 // build the component class
 class OdsToast extends LitElement {
-  // constructor() {
-  //   super();
+  constructor() {
+    super();
+    this.setAutoDismissal();
 
-  //   /*
-  //     If the component requires a touch detection,
-  //     please use this function to determine if a user is
-  //     activelly touching a device, versus detecting if
-  //     the device is touych enables or a handheld device.
+    //   /*
+    //     If the component requires a touch detection,
+    //     please use this function to determine if a user is
+    //     activelly touching a device, versus detecting if
+    //     the device is touch enables or a handheld device.
 
-  //     Also uncomment the touch detection lib above
-  //   */
-  //   this.addEventListener('touchstart', function() {
-  //     this.classList.add('is-touching');
-  //   });
-  // }
+    //     Also uncomment the touch detection lib above
+    //   */
+    //   this.addEventListener('touchstart', function() {
+    //     this.classList.add('is-touching');
+    //   });
+  }
 
   // function to define props used within the scope of thie component
   static get properties() {
@@ -38,13 +39,47 @@ class OdsToast extends LitElement {
     };
   }
 
+  destroy() {
+    console.log(this);
+    this.shadowRoot.querySelector(".toast").classList.add("exit");
+    setTimeout(() => {
+      this.parentElement.removeChild(this);
+    }, 1500);
+  }
+
+  setAutoDismissal() {
+    if (!!this.timeoutHandle) {
+      clearTimeout(this.timeoutHandle);
+    }
+    this.timeoutHandle = setTimeout(this.destroy.bind(this), 4000);
+  }
+
   // function that renders the HTML and CSS into  the scope of the component
   render() {
     return html`
       ${componentProperties} ${styleCss}
-      <div class="toast" aria-role="status">
-        <div class="title">${this.title}</div>
-        <div class="message">${this.message}</div>
+      <div
+        class="toast"
+        aria-role="status"
+        @click=${this.setAutoDismissal.bind(this)}
+      >
+        <div class="normal-content">
+          <div class="icon-container">
+            🦁
+          </div>
+          <div class="text-container">
+            <div class="title">${this.title}</div>
+            <div class="message">${this.message}</div>
+          </div>
+          <div class="exit-container">
+            <button @click=${this.destroy.bind(this)}>
+              🦄
+            </button>
+          </div>
+        </div>
+        <div class="action-container">
+          <slot></slot>
+        </div>
       </div>
     `;
   }
