db6466207a6a15b5197cdaf3cddd93fd8a2d8e64
James Jackson <james.jackson2@alaskaair.com>
2020-04-20 22:39:30 -0700

    test(eslint): add eslint and refactors to pass validation

--- src/toaster.js
+++ src/toaster.js
@@ -10,44 +10,49 @@ export default class Toaster {
 
   add(title, message, slotHTML) {
     const toast = document.createElement("ods-toast");
+
     toast.setAttribute("title", title);
     toast.setAttribute("message", message || "");
     toast.innerHTML = slotHTML || "";
-    toast.destroyCallback = this._destroyCurrentToast.bind(this);
-    toast.clickCallback = this._setAutoDismissal.bind(this);
+    toast.destroyCallback = this.destroyCurrentToast.bind(this);
+    toast.clickCallback = this.setAutoDismissal.bind(this);
 
     this.toasts.push(toast);
-    if (this.toasts.length == 1) {
-      this._showNextToast();
+    if (this.toasts.length === 1) {
+      this.showNextToast();
     }
   }
 
-  _showNextToast() {
+  showNextToast() {
     if (this.toasts.length > 0) {
       const currentToast = this.toasts[0];
+
       this.container.appendChild(currentToast);
       currentToast.classList.add("ods-toast__showToast");
       currentToast.tabIndex = 0;
-      currentToast.addEventListener("keydown", e => {
+      currentToast.addEventListener("keydown", (ex) => {
 
         // Supports either the esc or return keys
-        if (e.keyCode == 27 || e.keyCode == 13) {
-          this._destroyCurrentToast();
+        if (ex.keyCode === 27 || ex.keyCode === 13) {
+          this.destroyCurrentToast();
         }
       });
 
+      /* global Swipe */
+
       if (typeof Swipe !== "undefined") {
-        new Swipe(currentToast, this._destroyCurrentToast.bind(this));
+        new Swipe(currentToast, this.destroyCurrentToast.bind(this));
       }
 
-      this._setAutoDismissal();
+      this.setAutoDismissal();
     }
   }
 
    // animation-duration(css) must match _destroyCurrentToast() setTimeout value
-  _destroyCurrentToast(xDiff) {
+  destroyCurrentToast(xDiff) {
     if (this.toasts.length) {
       const currentToast = this.toasts[0];
+
       currentToast.classList.remove("ods-toast__showToast");
 
       if (xDiff < 0) {
@@ -61,17 +66,17 @@ export default class Toaster {
       setTimeout(() => {
         this.container.removeChild(currentToast);
         this.toasts.shift();
-        this._showNextToast();
+        this.showNextToast();
       }, 300);
     }
   }
 
-  _setAutoDismissal() {
-    if (!!this.timeoutHandle) {
+  setAutoDismissal() {
+    if (this.timeoutHandle) {
       clearTimeout(this.timeoutHandle);
     }
     this.timeoutHandle = setTimeout(
-      this._destroyCurrentToast.bind(this),
+      this.destroyCurrentToast.bind(this),
       this.displayTime
     );
   }
